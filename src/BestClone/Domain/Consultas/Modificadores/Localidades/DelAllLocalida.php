76cfa4ecd382a1aea489d7b2ca2151387fefe2e6
Javier Madueño <javiermadueno@gmail.com>
2016-02-23 11:59:56 +0100

    Modificadores Localidades

---
+++ src/BestClone/Domain/Consultas/Modificadores/Localidades/DelAllLocalida.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmadueno
+ * Date: 23/02/2016
+ * Time: 11:52
+ */
+
+namespace BestClone\Domain\Consultas\Modificadores\Localidades;
+
+
+class DelAllLocalidad {
+
+} 
---
+++ src/BestClone/Domain/Consultas/Modificadores/Localidades/ViewLocalidad.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jmadueno
+ * Date: 23/02/2016
+ * Time: 11:37
+ */
+
+namespace BestClone\Domain\Consultas\Modificadores\Localidades;
+
+
+class ViewLocalidad {
+
+} 
